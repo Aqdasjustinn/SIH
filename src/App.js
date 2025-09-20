549e2d4b96b97e6871a0bc10db052c36e1767e9c
Aqdas <165374772+Aqdasjustinn@users.noreply.github.com>
2025-09-21 01:15:08 +0530

    Fixed back App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
-import Settings from './pages/Settings';
-import Progress from './pages/Progress';
-import Courses from './pages/Courses';
 import Statistics from './pages/Statistics';
 import Activities from './pages/Activities';
 import './App.css';
@@ -14,12 +11,6 @@ function App() {
 
   const renderPage = () => {
     switch(activePage) {
-      case 'courses':
-        return <Courses />;
-      case 'progress':
-        return <Progress />;
-      case 'settings':
-        return <Settings />;
       case 'statistics':
         return <Statistics />;
       case 'activities':
